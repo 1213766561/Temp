3dd79d19114a72d80350629fb40a1d598ce99e56
1213766561 <1213766561@qq.com>
2021-06-09 02:01:59 +0800

    InitTempProject

---
+++ Source/TempProject/TempProject.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TempProject.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TempProject, "TempProject" );
---
+++ Source/TempProject/TempProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TempProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEMPPROJECT_API ATempProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
